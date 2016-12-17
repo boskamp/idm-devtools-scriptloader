5a4dce0a8eaa80728daec65c9e53b283a64d90b5
Lambert Boskamp <lambert@boskamp-consulting.com>
2016-12-17 12:33:44 +0100

    Add get/put cmds, improve usage, cleanup classpath

--- src/org/idmacs/IDMCLI.java
+++ src/org/idmacs/IDMCLI.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -14,7 +16,6 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.commons.codec.binary.Base64;
 
 public class IDMCLI {
@@ -23,25 +24,6 @@ public class IDMCLI {
 	private static void trc(String msg) {
 		if (g_verbose) {
 			System.err.println(msg);
-		}
-	}
-
-	private static void registerJdbcDrivers() {
-		try {
-			Class.forName("com.microsoft.jdbc.sqlserver.SQLServerDriver");
-		} catch (ClassNotFoundException e) {
-		}
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		} catch (ClassNotFoundException e) {
-		}
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		} catch (ClassNotFoundException e) {
-		}
-		try {
-			Class.forName("com.ibm.db2.jcc.DB2Driver");
-		} catch (ClassNotFoundException e) {
 		}
 	}
 
@@ -69,8 +51,8 @@ public class IDMCLI {
 		OutputStream os = new FileOutputStream(scriptFile);
 		String scriptBase64String = rs.getString(3);
 
-		if (scriptBase64String.startsWith("{b64}") || scriptBase64String.startsWith("{B64}")) {
-			scriptBase64String = scriptBase64String.substring("{b64}".length());
+		if (scriptBase64String.startsWith("{B64}")) {
+			scriptBase64String = scriptBase64String.substring("{B64}".length());
 		}
 
 		byte[] scriptUtf8Bytes = Base64.decodeBase64(scriptBase64String);
@@ -79,10 +61,9 @@ public class IDMCLI {
 		os.close();
 	}
 
-	private static void readScriptsFromDatabase(String jdbcUrl)
-			throws Exception {
-		final String M = "readScriptsFromDatabase: ";
-		trc(M + "Entering jdbcUrl=" + jdbcUrl);
+	private static void doDownload(String jdbcUrl, List<String> fileNames) throws Exception {
+		final String M = "doDownload: ";
+		trc(M + "Entering jdbcUrl=" + jdbcUrl+", fileNames="+fileNames);
 
 		Connection c = null;
 		Statement s = null;
@@ -121,7 +102,14 @@ public class IDMCLI {
 		}
 	}
 
+	private static void doUpload(String jdbcUrl, List<String> fileNames) throws Exception {
+		System.err.println("Not implemented yet");
+	}
+
 	public static void main(String[] args) throws Exception {
+		final String M = "main: ";
+		trc(M + "Entering args=" + args);
+
 		// create the command line parser
 		CommandLineParser parser = new DefaultParser();
 
@@ -133,7 +121,9 @@ public class IDMCLI {
 				.longOpt("url") // long name
 				.required() //
 				.desc("JDBC URL for _admin connection") // description
-				.hasArg().argName("JDBC_URL").build());
+				.hasArg() //
+				.argName("JDBC_URL") //
+				.build());
 
 		options.addOption(Option //
 				.builder("v") // short name
@@ -141,22 +131,87 @@ public class IDMCLI {
 				.required(false) //
 				.desc("Output trace messages to STDERR") // description
 				.build());
+
+		options.addOption(Option //
+				.builder("h") // short name
+				.longOpt("help") // long name
+				.required(false) //
+				.desc("Display this help") // description
+				.build());
+
+		boolean exceptionOccurred = false;
+		CommandLine line = null;
 		try {
 
 			// parse the command line arguments
-			CommandLine line = parser.parse(options, args);
+			line = parser.parse(options, args);
 			String jdbcUrl = line.getOptionValue('u');
 
 			g_verbose = line.hasOption('v');
 
-			registerJdbcDrivers();
-			readScriptsFromDatabase(jdbcUrl);
+			List<String> aCmd = line.getArgList();
+			if (aCmd.size() > 0) {
+				String cmd = aCmd.get(0).toLowerCase();
+				List<String> aFileNames = aCmd.subList(1, aCmd.size());
+				if ("get".startsWith(cmd)) {
+					doDownload(jdbcUrl, aFileNames);
+				} else if ("put".startsWith(cmd)) {
+					doUpload(jdbcUrl, aFileNames);
+				} else {
+					throw new Exception("Unrecognized command: " + cmd);
+				}
+			}
+			else {
+				throw new Exception("Missing command");
+			}
 
-		} catch (ParseException ex) {
-			System.err.println(ex.getMessage());
-			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp("idmcli", options);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			exceptionOccurred = true;
 		}
-	}
 
-}
+		if (exceptionOccurred || (line != null && line.hasOption('h'))) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.setLeftPadding(4);
+			String br = formatter.getNewLine();
+			char[] alp = new char[formatter.getLeftPadding()];
+			Arrays.fill(alp, ' ');
+			String lp = new String(alp);
+			String header = "Download or upload package scripts from SAP IDM database" + br //
+					+ br //
+					+ "COMMANDS:"+br //
+					+ lp + "get: download from database to filesystem" + br // 
+					+ lp + "put: upload from filesystem to database" + br //
+					+ br //
+					+ "OPTIONS:"+br //
+					;
+			
+			String footer = br // 
+					+"EXAMPLES:"+br //
+					+ "idmcli get de.foxysoft.core -u jdbc:sqlserver://..."+br //
+					+ lp + "Download all scripts from package de.foxysoft.core"+br //
+					+ lp + "into local directory $(pwd)/de.foxysoft.core, which will"+br //
+					+ lp + "be created if necessary."+br //
+					+ br //
+					+ "idmcli put de.foxsoft.core -u jdbc:sqlserver://..."+br //
+					+ lp + "Upload all *.js files located in directory $(pwd)/de.foxysoft.core"+br //
+					+ lp + "into package de.foxysoft.core in the database"+br //
+					+ br //
+					+ "idmcli put de.foxysoft.core/fx_trace.js     \\"+br //
+					+ "           de.foxysoft.core/fx_db_nolock.js \\"+br //
+					+ "        -u jdbc:sqlserver://..."+br //
+					+ lp + "Upload the two files fx_trace.js and fx_db_nolock.js,"+br //
+					+ lp + "stored in local directory $(pwd)/de.foxysoft.core,"+br //
+					+ lp + "into package de.foxysoft.core in the database"+br //
+					;
+
+			//Output options in the order they have been declared
+			formatter.setOptionComparator(null);
+			
+			formatter.printHelp("idmcli <cmd> <package>[/<script>]...", header, options, footer, true);
+		}
+
+		trc(M + "Returning");
+	}// main
+
+}// IDMCLI
